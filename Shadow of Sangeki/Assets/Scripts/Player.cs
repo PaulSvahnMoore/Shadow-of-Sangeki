7729dc436194e8a9c0ab33de4d89e0463805495d
Paul Svahn Moore <paul.svahnmoore@studerande.thegameassembly.se>
2021-02-27 23:03:59 +0100

    update

---
+++ Shadow of Sangeki/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate(){
+        moveInput = moveInput.GetAxisRaw("Horizontal")
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+}
