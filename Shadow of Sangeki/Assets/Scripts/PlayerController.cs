ca1b17c6b549f0969904e8758591ff1939cca1d2
Paul Svahn Moore <paul.svahnmoore@studerande.thegameassembly.se>
2021-02-28 00:41:24 +0100

    working character

--- Shadow of Sangeki/Assets/Scripts/PlayerController.cs
+++ Shadow of Sangeki/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,17 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb;
     public float speed;
+    public float jumpForce;
     private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
 
     void Start(){
         rb = GetComponent<Rigidbody2D>();
@@ -15,5 +25,40 @@ public class PlayerController : MonoBehaviour {
     void FixedUpdate(){
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Update(){
+
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+       
+        if(moveInput > 0) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        
+        } else if(moveInput < 0){
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space)){
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if(Input.GetKey(KeyCode.Space) && isJumping == true){
+
+            if(jumpTimeCounter > 0){
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            } else {
+                isJumping = false;
+
+            } 
+     
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space)){
+            isJumping = false; 
+
+        }
     }
 }
